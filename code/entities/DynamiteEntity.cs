eea00acf2da76c46e48d419086e247a8ab43d203
Delta135 <135.delta@gmail.com>
2021-09-14 19:29:53 +0200

    basic Dynamite Tool

---
+++ code/entities/DynamiteEntity.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[Library("ent_dynamite", Title = "Dynamite", Spawnable = false)]
+public partial class DynamiteEntity : Prop
+{
+	public override void Spawn()
+	{
+
+	}
+
+	public void Detonate()
+	{
+		Log.Info("BOOM!");
+
+		Particles.Create( "particles/explosion.vpcf", Position );
+		Sound.FromWorld( "sounds/common/explosions/explo_gas_can_01.vsnd", Position );
+
+		this.OnKilled();
+	}
+}
