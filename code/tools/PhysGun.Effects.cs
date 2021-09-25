ced6d89dfe66a527f590ee6a64cd9588f38d2056
Delta135 <135.delta@gmail.com>
2021-09-25 12:58:10 +0200

    Revert "Use component to change glow"
    This reverts commit 0bcd9887fa9e0235fa0455162d4c99ff95d0f697.

--- code/tools/PhysGun.Effects.cs
+++ code/tools/PhysGun.Effects.cs
@@ -1,5 +1,4 @@
 ﻿using Sandbox;
-using Sandbox.Component;
 using System.Linq;
 
 public partial class PhysGun
@@ -34,17 +33,12 @@ public partial class PhysGun
 				if ( child is Player )
 					continue;
 
-				if ( child.Components.TryGet<Glow>( out var childglow ) )
-				{
-					childglow.Active = false;
-				}
+				child.GlowActive = false;
+				child.GlowState = GlowStates.GlowStateOff;
 			}
 
-			if ( lastGrabbedEntity.Components.TryGet<Glow>( out var glow ) )
-			{
-				glow.Active = false;
-			}
-
+			lastGrabbedEntity.GlowActive = false;
+			lastGrabbedEntity.GlowState = GlowStates.GlowStateOff;
 			lastGrabbedEntity = null;
 		}
 	}
@@ -104,23 +98,22 @@ public partial class PhysGun
 			if ( GrabbedEntity is ModelEntity modelEnt )
 			{
 				lastGrabbedEntity = modelEnt;
-
-				var glow = modelEnt.Components.GetOrCreate<Glow>();
-				glow.Active = true;
-				glow.RangeMin = 0;
-				glow.RangeMax = 1000;
-				glow.Color = new Color( 0.1f, 1.0f, 1.0f, 1.0f );
+				modelEnt.GlowState = GlowStates.GlowStateOn;
+				modelEnt.GlowDistanceStart = 0;
+				modelEnt.GlowDistanceEnd = 1000;
+				modelEnt.GlowColor = new Color( 0.1f, 1.0f, 1.0f, 1.0f );
+				modelEnt.GlowActive = true;
 
 				foreach ( var child in lastGrabbedEntity.Children.OfType<ModelEntity>() )
 				{
 					if ( child is Player )
 						continue;
 
-					glow = child.Components.GetOrCreate<Glow>();
-					glow.Active = true;
-					glow.RangeMin = 0;
-					glow.RangeMax = 1000;
-					glow.Color = new Color( 0.1f, 1.0f, 1.0f, 1.0f );
+					child.GlowState = GlowStates.GlowStateOn;
+					child.GlowDistanceStart = 0;
+					child.GlowDistanceEnd = 1000;
+					child.GlowColor = new Color( 0.1f, 1.0f, 1.0f, 1.0f );
+					child.GlowActive = true;
 				}
 			}
 		}
